5ae563888da612aace5c9a2fd2e2b909e3e22e14
emil <bgemo105@gmail.com>
2023-07-05 12:03:54 +0300

    express adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+
+const expressApp = require('express')();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   app.enableCors();
   await app.listen(3000);
 }
