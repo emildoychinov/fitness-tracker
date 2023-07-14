53d4c010360c1f23ee5efe25547ca2375374b25a
emil <bgemo105@gmail.com>
2023-07-14 10:50:37 +0300

    server drop routine changed, now dropping database schema as well

--- drop_server.sh
+++ drop_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+SERVER=$(kubectl get pods -l app=server -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -it $SERVER -- bash -c "npm run typeorm -- -d ./typeOrm.config.ts schema:drop"
 kubectl delete service postgres-service server-service
 kubectl delete statefulset postgres-statefulset
 kubectl delete deployment server-deployment
-sleep 45s
+sleep 20s
 docker rmi fitness-tracker
 kubectl delete hpa server-deployment
